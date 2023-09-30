c258b48787fab89f7a8a33ab0ccbd1f12cc07468
minhnthfx21984 <minhnthfx21984@funix.edu.vn>
2023-09-30 12:17:59 +0700

    Remove my-folder from tracking

--- models/chatRoom.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const chatRoomSchema = new mongoose.Schema({
-  message: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }],
-});
-
-const ChatRoom = mongoose.model("ChatRoom", chatRoomSchema);
-
-module.exports = ChatRoom;
--- routes/email.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const checkAuth = require("../middleware/checkAuth");
-
-const emailController = require("../controllers/email");
-
-router.post("/email", checkAuth, emailController.sendEmail);
-
-module.exports = router;
